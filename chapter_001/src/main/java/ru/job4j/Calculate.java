ea35b8b558de8b016b5bc06a67737f73970d23e7
nik_sol <sol.of.f@mail.ru>
2018-02-02 21:56:09 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,14 @@
 package ru.job4j;
 
+/**
+* Простой класс для вывода в консоль
+* строки Hello World
+*/
 class Calculate {
+/**
+* Главный метод программы, содержащий функцию
+* вывода информации на консоль
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Nikolay Meleshkin (sol.of.f@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
