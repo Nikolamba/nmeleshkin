a9047a9696ad7e8c42bb0fbc81a488a302a55d5e
nik_sol <sol.of.f@mail.ru>
2018-02-08 21:45:41 +0300

    4.4. Построить пирамиду в псевдографике. [#4412]

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import java.util.StringJoiner;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author Nikolay Meleshkin (sol.of.f@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class PaintTest {
+    @Test
+    //проверяем пирамиду высотой 4
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    //проверяем пирамуду высотой 3
+    public void whenPyramid3Right() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(3);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("  ^  ")
+                                .add(" ^^^ ")
+                                .add("^^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    //проверяем отрицательные значения
+    public void whenPyramidNegative() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(-3);
+        System.out.println(rst);
+        assertThat(rst, is(""));
+    }
+}
