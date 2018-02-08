36589a35277b272c60f545247477a274d25a3ce6
nik_sol <sol.of.f@mail.ru>
2018-02-08 13:47:17 +0300

    3.4. Максимум из трех чисел [#190]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,14 +1,25 @@
 package ru.job4j.max;
 /**
- * Возвращает максимум из двух чисел.
  * @author Nikolay Meleshkin (sol.of.f@mail.ru)
  */
 public class Max {
     /**
+     * Возвращает максимум из двух чисел
      * @param first Первое число
      * @param second Второе число
      */
     public static int max(int first, int second) {
         return (first >= second ? first : second);
+    }
+    /**
+     * Возвращает максимум из трех чисел
+     * @param first Первое число
+     * @param second Второе число
+     * @param third Третье число
+     */
+    public static int max(int first, int second, int third) {
+        int result = max(first, second);
+        result = max(result, third);
+        return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -15,20 +15,51 @@ public class MaxTest {
     private int result;
 
     @Test
+    //max(int, int), когда первое число меньше второго
     public void whenFirstLessSecond() {
         result = Max.max(1, 2);
         assertThat(result, is(2));
     }
     @Test
+    //max(int, int), когда первое число больше второго
     public void whenFirstMoreSecond() {
         result = Max.max(3, 2);
         assertThat(result, is(3));
     }
 
     @Test
+    //max(int, int), когда первое число = второму
     public void whenFirstEqualSecond() {
         result = Max.max(4, 4);
         assertThat(result, is(4));
+    }
+
+    @Test
+    //max(int, int, int), когда первое число больше 2-х остальных
+    public void whenFirstLargest() {
+        result = Max.max(3, 2, 1);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    //max(int, int, int), когда второе число больше 2-х остальных
+    public void whenSecondLargest() {
+        result = Max.max(3, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    //max(int, int, int), когда третье число больше 2-х остальных
+    public void whenThirdLargest() {
+        result = Max.max(4, 5, 6);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    //max(int, int, int), когда числа равны
+    public void whenNumbersEqual() {
+        result = Max.max(10, 10, 10);
+        assertThat(result, is(10));
     }
 
 }
