dd36abbd80ae85e7c0b50763d8ac51ad01939286
nik_sol <sol.of.f@mail.ru>
2018-02-08 15:22:39 +0300

    4.2. Создать программу вычисляющую факториал. [#193]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+/**
+ * @author Nikolay Meleshkin (sol.of.f@mail.ru)
+ */
+public class Factorial {
+    /**
+     * метод подсчитывает факториал указанного числа
+     */
+    public int calc(int n) {
+        int result = 1;
+        if (n == 0) {
+            return 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
