b5673b825084fd0a1ee2b64639a2c3f735b6e9b3
nik_sol <sol.of.f@mail.ru>
2018-03-21 10:59:57 +0300

    2. Travis CI #31727

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -45,13 +45,4 @@ public class PaintTest {
                 )
         );
     }
-
-    @Test
-    //проверяем отрицательные значения
-    public void whenPyramidNegative() {
-        Paint paint = new Paint();
-        String rst = paint.pyramid(-3);
-        System.out.println(rst);
-        assertThat(rst, is(""));
-    }
 }
