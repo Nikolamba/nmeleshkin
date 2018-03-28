95712296b52530bfe1190c8fd809f78cea6cdfcb
nik_sol <sol.of.f@mail.ru>
2018-03-28 15:20:38 +0300

    Итератор для двухмерного массива int[][]

---
+++ chapter_004/src/main/java/ru/job4j/MatrixIterator.java
@@ -0,0 +1,39 @@
+package ru.job4j;
+
+import java.util.Iterator;
+
+/**
+ * @author Nikolay Meleshkin (sol.of.f@mail.ru)
+ * @version 0.1
+ */
+public class MatrixIterator implements Iterator<Integer> {
+
+    private int[] massiv;
+    private int position = 0;
+
+    MatrixIterator(int[][] massiv) {
+        int lenghtMas = 0;
+        for (int[] subMas : massiv) {
+            lenghtMas += subMas.length;
+        }
+        int[] result = new int[lenghtMas];
+        int index = 0;
+        for (int[] subMas : massiv) {
+            for (int value : subMas) {
+                result[index++] = value;
+            }
+        }
+        this.massiv = result;
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return (massiv.length > position);
+    }
+
+    @Override
+    public Integer next() {
+        return massiv[position++];
+    }
+}
