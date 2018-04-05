94a213f88a913eaefb6bc5cd7f37830c0051240d
nik_sol <sol.of.f@mail.ru>
2018-04-05 12:16:31 +0300

    5.1.1. Итератор для двухмерного массива int[][] [#9539]

--- chapter_004/src/main/java/ru/job4j/MatrixIterator.java
+++ chapter_004/src/main/java/ru/job4j/MatrixIterator.java
@@ -7,37 +7,32 @@ import java.util.NoSuchElementException;
  * @author Nikolay Meleshkin (sol.of.f@mail.ru)
  * @version 0.1
  */
+
 public class MatrixIterator implements Iterator<Integer> {
 
-    private int[] massiv;
+    private int[][] massiv;
     private int position = 0;
+    private int subPostion = 0;
 
     MatrixIterator(final int[][] massiv) {
-        int lenghtMas = 0;
-        for (int[] subMas : massiv) {
-            lenghtMas += subMas.length;
-        }
-        int[] result = new int[lenghtMas];
-        int index = 0;
-        for (int[] subMas : massiv) {
-            for (int value : subMas) {
-                result[index++] = value;
-            }
-        }
-        this.massiv = result;
-
+        this.massiv = massiv;
     }
 
     @Override
     public boolean hasNext() {
-        return (position < massiv.length);
+        return ((position != massiv.length - 1)
+                || (subPostion != massiv[position].length));
     }
 
     @Override
     public Integer next() {
-        if (position >= massiv.length) {
+        if (!hasNext() || massiv.length == 0) {
             throw new NoSuchElementException();
         }
-        return massiv[position++];
+        if (subPostion == massiv[position].length) {
+            position++;
+            subPostion = 0;
+        }
+        return massiv[position][subPostion++];
     }
 }
