04bd29b1b2ab03cceb145450bf476302a6ba6f2e
nik_sol <sol.of.f@mail.ru>
2018-04-15 20:45:39 +0300

    5. Перекрывать и equals и hashCode [#1002]

--- chapter_004/src/test/java/ru/job4j/map/WithEqualsAndHashCodeTest.java
+++ chapter_004/src/test/java/ru/job4j/map/WithEqualsAndHashCodeTest.java
@@ -9,18 +9,18 @@ import java.util.Map;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class WithoutHashCodeWithEqualsTest {
+public class WithEqualsAndHashCodeTest {
 
     private Map<User, Object> map = new HashMap<>();
 
     @Test
-    public void whenAddSameElementsShouldAddAllElements() {
+    public void whenAddSameElementsShouldAddOneElement() {
         User us1 = new User("Igor", new GregorianCalendar(1984, 1, 12), 1);
         User us2 = new User("Igor", new GregorianCalendar(1984, 1, 12), 1);
-        map.put(us1, "Igor");
-        map.put(us2, "Igor");
+        map.put(us1, "Igor_1");
+        map.put(us2, "Igor_2");
         System.out.println(map);
-        assertThat(map.size(), is(2));
+        assertThat(map.size(), is(1));
     }
 
 }
