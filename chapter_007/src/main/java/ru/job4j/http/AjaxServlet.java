747625dd61b7631c49be5059855984e5310214ee
nik_sol <sol.of.f@mail.ru>
2018-09-04 12:17:31 +0300

    4. Ajax Jquery [#58534]

---
+++ chapter_007/src/main/java/ru/job4j/http/AjaxServlet.java
@@ -0,0 +1,44 @@
+package ru.job4j.http;
+
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.type.TypeReference;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author Nikolay Meleshkin (sol.of.f@mail.ru)
+ * @version 0.1
+ */
+public class AjaxServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/ajax.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, String> map = mapper.readValue(sb.toString(), new TypeReference<Map<String, String>>() { });
+        PrintWriter writer = resp.getWriter();
+        writer.append("<tr><td>");
+        writer.append(map.get("name"));
+        writer.append("</td><td>");
+        writer.append(map.get("lastname"));
+        writer.append("</td></tr>");
+        writer.flush();
+    }
+}
